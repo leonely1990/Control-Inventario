ba0c05b7472777302e58068c10577fd62dc9a8de
leonely1990 <leonel.wow.1990@gmail.com>
2022-10-11 00:33:39 -0400

    Modificaciones a nombres de archivos css, algunas correxiones en los mismos y en clases de articulos

--- Front-End/js/panel.js
+++ Front-End/js/panel.js
@@ -1,12 +1,28 @@
 const boton_mobil = document.getElementById("boton-menu-mobil");
 const boton_cerrar_barra = document.getElementById("cerrar-barra-lateral");
 const barra_lateral = document.getElementById("barra-lateral");
-
+const tipos = document.getElementsByClassName("tipo");
 
 boton_mobil.addEventListener("click", () => {
     barra_lateral.style.left = "0";
 });
 
+
 boton_cerrar_barra.addEventListener("click" , () => {
     barra_lateral.style.left = "-130px";
 });
+
+
+for(let tipo of tipos){
+    tipo.childNodes[1].addEventListener("click", () => {
+        
+        if(tipo.childNodes[3].classList == "lista"){
+            tipo.childNodes[1].childNodes[3].src = "Front-End/iconos/angle-up.svg";
+            tipo.childNodes[3].classList = "lista active";
+        }
+        else if(tipo.childNodes[3].classList == "lista active"){
+            tipo.childNodes[3].classList = "lista";
+            tipo.childNodes[1].childNodes[3].src = "Front-End/iconos/angle-down.svg";
+        }
+    });
+}
